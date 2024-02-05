6e96b321929c53eee74021c91815676e7cc5547a
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-02-06 00:02:25 +0530

    feat: projects-section

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -4,10 +4,14 @@ const Projects = () => {
   return (
     <div className="my-12 flex w-full flex-col items-center justify-center">
       <MotionDiv delayOffset={0.2}>
-        <h2 className="text-2xl font-semibold md:text-3xl mb-3 md:mb-6">Projects</h2>
+        <h2 className="text-2xl font-semibold md:text-3xl mb-3 md:mb-6">
+          Projects
+        </h2>
       </MotionDiv>
-      
-              
+
+    <div className="grid grid-cols-1 md:grid-cols-2 ">
+
+    </div>
     </div>
   );
 };
