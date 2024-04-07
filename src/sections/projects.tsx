f969bea3506ce607b7ce7b9db6b4f7936a584897
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-07 23:19:14 +0530

    refractor: projects

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -7,24 +7,27 @@ const Projects = () => {
   return (
     <MotionDiv delayOffset={0.2}>
       <div className="my-8 md:my-12 w-full md:max-w-screen-2xl md:mx-auto md:px-16">
-      <ul className="flex flex-col items-center justify-center w-full">
-        {projectsContent.map((card, idx) => {
-          return (
-            <div key={idx}>
-              <ProjectCard
-                title={card.title}
-                header={card.header}
-                description={card.description}
-                tags={card.tags}
-                link={card.link}
-              />
+        <div className="flex justify-center items-center text-3xl font-semibold">
+          Projects
+        </div>
+        <ul className="flex flex-col items-center justify-center w-full">
+          {projectsContent.map((card, idx) => {
+            return (
+              <div key={idx}>
+                <ProjectCard
+                  title={card.title}
+                  header={card.header}
+                  description={card.description}
+                  tags={card.tags}
+                  link={card.link}
+                />
 
-              <Separator className="my-4" />
-            </div>
-          );
-        })}
-      </ul>
-    </div>
+                <Separator className="my-4" />
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </MotionDiv>
   );
 };
