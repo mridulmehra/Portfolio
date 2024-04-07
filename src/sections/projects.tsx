b6f6e807980aa26d4ba92086f7eae42aded4f4d4
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-07 23:12:20 +0530

    feat: projects

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -1,18 +1,31 @@
 import MotionDiv from "@/components/motion-div";
+import ProjectCard from "@/components/projects/project-card";
+import { projectsContent } from "@/components/projects/project-content";
+import { Separator } from "@/components/ui/separator";
 
 const Projects = () => {
   return (
-    <div className="my-12 flex w-full flex-col items-center justify-center">
-      <MotionDiv delayOffset={0.2}>
-        <h2 className="text-2xl font-semibold md:text-3xl mb-3 md:mb-6">
-          Projects
-        </h2>
-      </MotionDiv>
+    <MotionDiv delayOffset={0.2}>
+      <div className="my-8 md:my-12 w-full md:max-w-screen-2xl md:mx-auto md:px-16">
+      <ul className="flex flex-col items-center justify-center w-full">
+        {projectsContent.map((card, idx) => {
+          return (
+            <div key={idx}>
+              <ProjectCard
+                title={card.title}
+                header={card.header}
+                description={card.description}
+                tags={card.tags}
+                link={card.link}
+              />
 
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div>hello</div>
-      </div>
+              <Separator className="my-4" />
+            </div>
+          );
+        })}
+      </ul>
     </div>
+    </MotionDiv>
   );
 };
 
